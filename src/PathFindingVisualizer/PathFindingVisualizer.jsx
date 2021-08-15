5bb0f688443b84e3557f19144a665ada228ece65
David Duval <de.duval6@gmail.com>
2021-08-14 20:45:04 -0700

    Set up default jsx files

---
+++ src/PathFindingVisualizer/PathFindingVisualizer.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import  Node  from "./Node/Node";
+
+import "./PathFindingVisualizer.css"
+
+export default class PathFidningVisualizer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <div> 
+            foo
+            <Node></Node>
+            </div>
+        );
+    }
+
+
+}
